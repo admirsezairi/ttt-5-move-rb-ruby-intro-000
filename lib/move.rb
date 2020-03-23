65c3644bfdc28c082410cfc480bb9d1f942e92a6
Admir Sezairi <sezairiadmir@gmail.com>
2020-03-23 03:32:16 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,3 +11,20 @@ board = [" ", " ", " "]
 def update_array_at_with(array, index, value)
   array[index] = value
 end
+def number_adder(n)
+  n += 10
+end
+
+def array_adder(a)
+  a << "new thing at the end of the array"
+end
+
+x = 10
+puts "Before call #{x}"
+number_adder(x)
+puts "After call: #{x}: Holy moly, unchanged!"
+
+z = [1, 'hi', "Byron"]
+puts "Before call #{z}"
+array_adder(z)
+puts "After call #{z}: Holy moly, *changed*!"
